b52bca49ed9a31c4c51ae0b950871ccb2904073c
Haystack <tusharforever@gmail.com>
2019-03-10 22:59:27 -0400

    Create linkedlist_L2_E7.py

---
+++ linkedlist_L2_E7.py
@@ -0,0 +1,123 @@
+"""The LinkedList code from before is provided below.
+Add three functions to the LinkedList.
+"get_position" returns the element at a certain position.
+The "insert" function will add an element to a particular
+spot in the list.
+"delete" will delete the first element with that
+particular value.
+Then, use "Test Run" and "Submit" to run the test cases
+at the bottom."""
+
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
+            
+    def get_position(self, position):
+        """Get an element from a particular position.
+        Assume the first position is "1".
+        Return "None" if position is not in the list."""
+        current = self.head
+        count = 1
+        if self.head:
+            while count <= position:
+                if count == position:
+                    return current
+                else:
+                    current = current.next
+                    count += 1
+        else: 
+            return None
+    
+    def insert(self, new_element, position):
+        """Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements."""
+        if position > 0:
+            if position == 1:
+                new_element.next = self.head
+                self.head = new_element
+            else:
+                current = self.get_position(position)
+                prev = self.get_position(position - 1)
+                prev.next = new_element
+                new_element.next = current
+
+    
+    def delete(self, value):
+        """Delete the first node with a given value."""
+        current = self.head
+        count = 1
+        if self.head:
+            while current:
+                if current.value == value:
+                    print "Match"
+                    if count == 1:
+                        nextEle = self.get_position(count + 1)
+                        self.head = nextEle
+                    else:    
+                        prev = self.get_position(count - 1)
+                        nextEle = self.get_position(count + 1)
+                        prev.next = nextEle
+                    return
+                else:
+                    current = current.next
+                    count += 1
+                    
+        
+        
+    def print_list(self):
+        current = self.head
+        if self.head:
+            print "Printing List"
+            while current:
+                print current.value
+                current = current.next
+        print "End of List"
+
+
+# Test cases
+# Set up some Elements
+e1 = Element(1)
+e2 = Element(2)
+e3 = Element(3)
+e4 = Element(4)
+
+# Start setting up a LinkedList
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
+
+# Test get_position
+# Should print 3
+print ll.head.next.next.value
+# Should also print 3
+print ll.get_position(3).value
+
+# Test insert
+ll.insert(e4,3)
+# Should print 4 now
+print ll.get_position(3).value
+
+# Test delete
+ll.delete(1)
+# Should print 2 now
+print ll.get_position(1).value
+# Should print 4 now
+print ll.get_position(2).value
+# Should print 3 now
+print ll.get_position(3).value
